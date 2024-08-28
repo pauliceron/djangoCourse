bcb459cb3cb6303a70d9faf3edb48d5abb247135
Paulina Cerón <pceronm@eafit.edu.co>
2024-08-27 19:38:41 -0500

    Workshop 3: Add and remove products from cart

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
  path('products/create', ProductCreateView.as_view(), name='form'),
  path('products/success', SuccessPageView.as_view(), name='success'),
  path('products/<str:id>', ProductShowView.as_view(), name='show'),
+ path('cart/', CartView.as_view(), name='cart_index'), 
+ path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+ path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
 ] 
